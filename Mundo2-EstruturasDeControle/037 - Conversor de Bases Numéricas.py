8660b6afe30bcf85e18883a154445738f1c51d3f
jonasht <jhenriquet@outlook.com.br>
2020-08-24 21:55:34 -0300

    add

--- Mundo2-EstruturasDeControle/037 - Conversor de Bases Numéricas.py
+++ Mundo2-EstruturasDeControle/037 - Conversor de Bases Numéricas.py
@@ -1,13 +1,16 @@
+import colorama 
+colorama.init()
+
 #Exercício Python 037:
 # Escreva um programa em Python que leia um número inteiro qualquer e peça para o usuário escolher qual será a base de conversão:
 #  1 para binário,
 # 2 para octal e
 # 3 para hexadecimal.
 print('\33[7;32m  ')
-valor = int(input('\33[mQual é o valor decimal para converter? :'))
-op = int(input('''1 binario
-2 octal
-3 hexadecimal
+valor = int(input('Qual é o valor decimal para converter? :'))
+op = int(input('''\t1-binario
+        2-octal
+        3-hexadecimal
 opção:'''))
 if op == 1:
     print('Binario {}'.format(bin(valor)[2:]))
